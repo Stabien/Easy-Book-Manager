f814ecfbab862d721fa4a6e78380ac6190ef013c
Stabien <bastien.piedallu@gmail.com>
2021-01-21 11:15:25 +0100

    commit projet folder before pull

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -40,10 +40,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(138, 37);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(101, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(230, 32);
+            this.label1.Size = new System.Drawing.Size(287, 41);
             this.label1.TabIndex = 0;
             this.label1.Text = "Easy Book Manager";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -52,20 +51,20 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(139, 125);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Location = new System.Drawing.Point(96, 129);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(232, 26);
+            this.textBox1.Size = new System.Drawing.Size(308, 30);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(139, 194);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Location = new System.Drawing.Point(96, 214);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(232, 26);
+            this.textBox2.Size = new System.Drawing.Size(308, 30);
             this.textBox2.TabIndex = 2;
             this.textBox2.UseSystemPasswordChar = true;
             // 
@@ -73,10 +72,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(135, 100);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(91, 98);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(131, 21);
+            this.label2.Size = new System.Drawing.Size(164, 28);
             this.label2.TabIndex = 3;
             this.label2.Text = "Nom d\'utilisateur";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -85,10 +83,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(135, 169);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(91, 183);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(102, 21);
+            this.label3.Size = new System.Drawing.Size(129, 28);
             this.label3.TabIndex = 4;
             this.label3.Text = "Mot de passe";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -96,10 +93,10 @@
             // Valider
             // 
             this.Valider.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Valider.Location = new System.Drawing.Point(203, 247);
-            this.Valider.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Valider.Location = new System.Drawing.Point(181, 269);
+            this.Valider.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Valider.Name = "Valider";
-            this.Valider.Size = new System.Drawing.Size(98, 32);
+            this.Valider.Size = new System.Drawing.Size(131, 39);
             this.Valider.TabIndex = 5;
             this.Valider.Text = "Valider";
             this.Valider.UseVisualStyleBackColor = true;
@@ -107,16 +104,16 @@
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 334);
+            this.ClientSize = new System.Drawing.Size(495, 343);
             this.Controls.Add(this.Valider);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Login";
             this.Text = "Easy Book Manager";
             this.Load += new System.EventHandler(this.Form1_Load);
