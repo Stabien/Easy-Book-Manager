9732038b3cd7faaedb97fb6c0670299bb6f5227a
Yann <honza.doan@gmail.com>
2021-02-04 10:48:47 +0100

    Ajout_Adherent

---
+++ Ajout_Adherent.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Easy_Book_Manager
+{
+    partial class Ajout_Adherent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_ajout = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox_Adresse_ad = new System.Windows.Forms.TextBox();
+            this.label_Adress_ad = new System.Windows.Forms.Label();
+            this.textBox_Tel_ad = new System.Windows.Forms.TextBox();
+            this.label_Tel_ad = new System.Windows.Forms.Label();
+            this.textBox_Age_ad = new System.Windows.Forms.TextBox();
+            this.label_Age_ad = new System.Windows.Forms.Label();
+            this.textBox_Prenom_ad = new System.Windows.Forms.TextBox();
+            this.label_Prenom_ad = new System.Windows.Forms.Label();
+            this.textBox_Nom_ad = new System.Windows.Forms.TextBox();
+            this.label_Nom_ad = new System.Windows.Forms.Label();
+            this.button_Valider_ad = new System.Windows.Forms.Button();
+            this.button_Annuler_ad = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label_ajout
+            // 
+            this.label_ajout.AutoSize = true;
+            this.label_ajout.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_ajout.Location = new System.Drawing.Point(49, 41);
+            this.label_ajout.Name = "label_ajout";
+            this.label_ajout.Size = new System.Drawing.Size(388, 54);
+            this.label_ajout.TabIndex = 1;
+            this.label_ajout.Text = "Ajouter un Adhérent";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox_Adresse_ad);
+            this.panel1.Controls.Add(this.label_ajout);
+            this.panel1.Controls.Add(this.label_Adress_ad);
+            this.panel1.Controls.Add(this.textBox_Tel_ad);
+            this.panel1.Controls.Add(this.label_Tel_ad);
+            this.panel1.Controls.Add(this.textBox_Age_ad);
+            this.panel1.Controls.Add(this.label_Age_ad);
+            this.panel1.Controls.Add(this.textBox_Prenom_ad);
+            this.panel1.Controls.Add(this.label_Prenom_ad);
+            this.panel1.Controls.Add(this.textBox_Nom_ad);
+            this.panel1.Controls.Add(this.label_Nom_ad);
+            this.panel1.Location = new System.Drawing.Point(41, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(491, 815);
+            this.panel1.TabIndex = 2;
+            // 
+            // textBox_Adresse_ad
+            // 
+            this.textBox_Adresse_ad.Location = new System.Drawing.Point(58, 731);
+            this.textBox_Adresse_ad.Multiline = true;
+            this.textBox_Adresse_ad.Name = "textBox_Adresse_ad";
+            this.textBox_Adresse_ad.Size = new System.Drawing.Size(325, 33);
+            this.textBox_Adresse_ad.TabIndex = 15;
+            // 
+            // label_Adress_ad
+            // 
+            this.label_Adress_ad.AutoSize = true;
+            this.label_Adress_ad.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Adress_ad.Location = new System.Drawing.Point(53, 672);
+            this.label_Adress_ad.Name = "label_Adress_ad";
+            this.label_Adress_ad.Size = new System.Drawing.Size(148, 30);
+            this.label_Adress_ad.TabIndex = 14;
+            this.label_Adress_ad.Text = "Adresse postal";
+            // 
+            // textBox_Tel_ad
+            // 
+            this.textBox_Tel_ad.Location = new System.Drawing.Point(58, 589);
+            this.textBox_Tel_ad.Multiline = true;
+            this.textBox_Tel_ad.Name = "textBox_Tel_ad";
+            this.textBox_Tel_ad.Size = new System.Drawing.Size(325, 33);
+            this.textBox_Tel_ad.TabIndex = 13;
+            // 
+            // label_Tel_ad
+            // 
+            this.label_Tel_ad.AutoSize = true;
+            this.label_Tel_ad.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Tel_ad.Location = new System.Drawing.Point(53, 533);
+            this.label_Tel_ad.Name = "label_Tel_ad";
+            this.label_Tel_ad.Size = new System.Drawing.Size(217, 30);
+            this.label_Tel_ad.TabIndex = 12;
+            this.label_Tel_ad.Text = "Numéro de téléphone";
+            // 
+            // textBox_Age_ad
+            // 
+            this.textBox_Age_ad.Location = new System.Drawing.Point(58, 448);
+            this.textBox_Age_ad.Multiline = true;
+            this.textBox_Age_ad.Name = "textBox_Age_ad";
+            this.textBox_Age_ad.Size = new System.Drawing.Size(325, 33);
+            this.textBox_Age_ad.TabIndex = 11;
+            // 
+            // label_Age_ad
+            // 
+            this.label_Age_ad.AutoSize = true;
+            this.label_Age_ad.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Age_ad.Location = new System.Drawing.Point(53, 399);
+            this.label_Age_ad.Name = "label_Age_ad";
+            this.label_Age_ad.Size = new System.Drawing.Size(50, 30);
+            this.label_Age_ad.TabIndex = 10;
+            this.label_Age_ad.Text = "Âge";
+            // 
+            // textBox_Prenom_ad
+            // 
+            this.textBox_Prenom_ad.Location = new System.Drawing.Point(58, 335);
+            this.textBox_Prenom_ad.Multiline = true;
+            this.textBox_Prenom_ad.Name = "textBox_Prenom_ad";
+            this.textBox_Prenom_ad.Size = new System.Drawing.Size(325, 33);
+            this.textBox_Prenom_ad.TabIndex = 9;
+            // 
+            // label_Prenom_ad
+            // 
+            this.label_Prenom_ad.AutoSize = true;
+            this.label_Prenom_ad.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Prenom_ad.Location = new System.Drawing.Point(53, 281);
+            this.label_Prenom_ad.Name = "label_Prenom_ad";
+            this.label_Prenom_ad.Size = new System.Drawing.Size(85, 30);
+            this.label_Prenom_ad.TabIndex = 8;
+            this.label_Prenom_ad.Text = "Prénom";
+            // 
+            // textBox_Nom_ad
+            // 
+            this.textBox_Nom_ad.Location = new System.Drawing.Point(58, 214);
+            this.textBox_Nom_ad.Multiline = true;
+            this.textBox_Nom_ad.Name = "textBox_Nom_ad";
+            this.textBox_Nom_ad.Size = new System.Drawing.Size(325, 33);
+            this.textBox_Nom_ad.TabIndex = 7;
+            // 
+            // label_Nom_ad
+            // 
+            this.label_Nom_ad.AutoSize = true;
+            this.label_Nom_ad.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Nom_ad.Location = new System.Drawing.Point(53, 157);
+            this.label_Nom_ad.Name = "label_Nom_ad";
+            this.label_Nom_ad.Size = new System.Drawing.Size(59, 30);
+            this.label_Nom_ad.TabIndex = 6;
+            this.label_Nom_ad.Text = "Nom";
+            // 
+            // button_Valider_ad
+            // 
+            this.button_Valider_ad.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Valider_ad.Location = new System.Drawing.Point(78, 927);
+            this.button_Valider_ad.Name = "button_Valider_ad";
+            this.button_Valider_ad.Size = new System.Drawing.Size(164, 49);
+            this.button_Valider_ad.TabIndex = 3;
+            this.button_Valider_ad.Text = "Valider";
+            this.button_Valider_ad.UseVisualStyleBackColor = true;
+            // 
+            // button_Annuler_ad
+            // 
+            this.button_Annuler_ad.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Annuler_ad.Location = new System.Drawing.Point(314, 927);
+            this.button_Annuler_ad.Name = "button_Annuler_ad";
+            this.button_Annuler_ad.Size = new System.Drawing.Size(164, 49);
+            this.button_Annuler_ad.TabIndex = 4;
+            this.button_Annuler_ad.Text = "Annuler";
+            this.button_Annuler_ad.UseVisualStyleBackColor = true;
+            // 
+            // Ajout_Adherent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 1020);
+            this.Controls.Add(this.button_Annuler_ad);
+            this.Controls.Add(this.button_Valider_ad);
+            this.Controls.Add(this.panel1);
+            this.Name = "Ajout_Adherent";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_ajout;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox_Adresse_ad;
+        private System.Windows.Forms.Label label_Adress_ad;
+        private System.Windows.Forms.TextBox textBox_Tel_ad;
+        private System.Windows.Forms.Label label_Tel_ad;
+        private System.Windows.Forms.TextBox textBox_Age_ad;
+        private System.Windows.Forms.Label label_Age_ad;
+        private System.Windows.Forms.TextBox textBox_Prenom_ad;
+        private System.Windows.Forms.Label label_Prenom_ad;
+        private System.Windows.Forms.TextBox textBox_Nom_ad;
+        private System.Windows.Forms.Label label_Nom_ad;
+        private System.Windows.Forms.Button button_Valider_ad;
+        private System.Windows.Forms.Button button_Annuler_ad;
+    }
+}
---
+++ Ajout_Adherent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Easy_Book_Manager
+{
+    public partial class Ajout_Adherent : Form
+    {
+        public Ajout_Adherent()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
