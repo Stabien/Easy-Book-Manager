547b7ba2ff6a3662ac0999e245b412061606807a
Yann <honza.doan@gmail.com>
2021-04-12 11:41:36 +0200

    Page Emprunt & Gerer Emprunt qui s'actualise a chaques boutons enregistrer

--- Regroupement_Emprunt.cs
+++ Regroupement_Emprunt.cs
@@ -56,10 +56,23 @@ namespace Easy_Book_Manager
         {
             try
             {
+                //Netoyage des listbox et text pour les nouvelles requetes de la page Gerer Emprunt
                 NomAdherent.Text = "";
                 AdresseAdherent.Text = "";
                 PrenomAdherent.Text = "";
                 TelephoneAdherent.Text = "";
+                listBoxAdherent.Items.Clear();
+                listeNom.Clear();
+                //Netoyage des listbox/text de la page emprunt
+                nAdherent.Text = "";
+                pAdherent.Text = "";
+                aAdherent.Text = "";
+                tAdherent.Text = "";
+                listBoxLivres.Items.Clear();
+                listeLivres.Clear();
+                ListeAdherentGererEmprunt.Items.Clear();
+                listeAdherent.Clear();
+
 
                 //Permet la connexion à la bdd
 
@@ -701,6 +714,9 @@ namespace Easy_Book_Manager
                 MessageBox.Show(ex.Message);
             }
 
+            //Update de la page avec les nouvelles
+            Base();
+
         }
         //--------------------------------------------------------------------------------------//
 
@@ -1078,55 +1094,10 @@ namespace Easy_Book_Manager
 
                     }
                     MessageBox.Show("Emprunt termine!");
-                    if(dbConn != null)
-                        dbConn.Close();
-                    nAdherent.Text = "";
-                    pAdherent.Text = "";
-                    aAdherent.Text = "";
-                    tAdherent.Text = "";
-                    listBoxLivres.Items.Clear();
-                    ListeAdherentGererEmprunt.Items.Clear();
-                    Liste_Livre_Emprunt.Items.Clear();
-                    dbConn.Open();
 
-                    //Creation requete SQL
-                    LivresCommand = new SqlCommand
-                        (
-                            "Select ID, Titre, Auteur, Emprunte from Livres where Emprunte = 0", dbConn
-                        );
+                    //Update de la page avec les nouvelles
+                    Base();
 
-                    reader = LivresCommand.ExecuteReader();
-
-                    while (reader.Read())
-                    {
-                        //Remplis la listBoxLivres avec des livres
-
-                        listBoxLivres.Items.Add(reader["ID"] + " " + reader["Titre"] + " " + reader["Auteur"]);
-                        listeLivres.Add(reader["Titre"] + "  " + reader["Auteur"] + " " + reader["Emprunte"]);
-                    }
-                    if (reader != null)
-                        reader.Close();
-                    if (dbConn != null)
-                        dbConn.Close();
-
-                    dbConn.Open();
-
-                    GerrerAdherentCommand = new SqlCommand
-                    (
-                       "Select Nom, id, Prenom from Adherents where Emprunt_en_cours is NULL", dbConn
-                    );
-
-                    reader = GerrerAdherentCommand.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        //Remplis la listbox ListeAdherentGererEmprunt des adherents
-                        ListeAdherentGererEmprunt.Items.Add(reader["id"] + " " + reader["Nom"] + " " + reader["Prenom"]);
-                        listeAdherent.Add(reader["id"] + " " + reader["Nom"] + " " + reader["Prenom"]);
-                    }
-                    if (reader != null)
-                        reader.Close();
-                    if (dbConn != null)
-                        dbConn.Close();
                 }
                 catch (Exception ex)
                 {
